cac7044f6ece91931f3a7c404dce90b1ed6b7e2e
Kanyisa Ntombini <kanyisa.ntombini@gmail.com>
2024-01-14 19:02:03 +0200

    Added hooks to search bar

--- src/components/SearchBar/SearchBar.js
+++ src/components/SearchBar/SearchBar.js
@@ -1,9 +1,32 @@
-import React from 'react';
+import { useState } from 'react';
+import './SearchBar.css';
+import SearchResults from '../SearchResults/SearchResults';
+import filterSongData from '../../spotify_api.js';
 
 function SearchBar() {
+  const [hasReceivedData, setHasReceivedData] = useState(false);
+  const [songTracks, setSongTracks] = useState([]);
+
+  const handleClick = () => {
+    let searchSong = document.getElementById('searchSong');
+    searchSong = searchSong.value || '';
+    const songs = filterSongData(searchSong);
+
+    if (songs) {
+      setHasReceivedData(true);
+      setSongTracks(songs);
+    }
+  };
+
   return (
-    <section>
-      <div>I am search bar</div>
+    <section className="SearchBar">
+      <input
+        id="searchSong"
+        type="text"
+        placeholder="Please enter a song title..."
+      />
+      <button onClick={handleClick}>Search</button>
+      {hasReceivedData && <SearchResults songTracks={songTracks} />}
     </section>
   );
 }
