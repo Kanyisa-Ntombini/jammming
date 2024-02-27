e2a795d707a9b50876938288b2be07524267ebcb
Kanyisa Ntombini <kanyisa.ntombini@gmail.com>
2024-02-27 18:08:58 +0200

    Created new Footer component

---
+++ src/components/Footer/Footer.js
@@ -0,0 +1,11 @@
+import './Footer.css';
+
+function Footer() {
+  return (
+    <footer className="Footer">
+      <div>&copy; 2024 K Ntombini</div>
+    </footer>
+  );
+}
+
+export default Footer;
